7932354a3015dc2981f7746051d3375961a1af63
Antonio Rafael Nieto Mora <anm020@inlumine.ual.es>
2026-05-30 17:10:09 +0200

    Fix: arreglo puertos para selenium

--- e2e/src/utils/browser.setup.ts
+++ e2e/src/utils/browser.setup.ts
@@ -1,7 +1,7 @@
 import { Builder, ThenableWebDriver } from "selenium-webdriver";
 import chrome from "selenium-webdriver/chrome";
 
-export const APP_URL = "http://localhost:4200";
+export const APP_URL = process.env.APP_URL || "http://localhost:4200";
 
 export class BrowserSetup {
   static async buildDriver(): Promise<ThenableWebDriver> {
